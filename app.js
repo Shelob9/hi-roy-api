ea05f52935fda6cbd67244cef08de77729970c32
Rachel Carden <bamadesigner@gmail.com>
2016-03-14 16:45:46 -0500

    Adding a handler to redirect to https, if necessary

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
+// Redirect to https, if necessary
+app.use(function (req, res, next) {
+	if ('https'==req.get('x-forwarded-protocol')) {
+		next();
+	} else {
+		res.redirect('https://' + req.headers.host + req.url);
+    }
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
